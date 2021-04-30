48f3f7f4798c947cb898567f24fb0bf470c50ce3
atsushi570 <55192393+atsushi570@users.noreply.github.com>
2021-04-30 21:59:38 +0900

    テスト

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ function main() {
 
   // json serverを起動する
   server.use(router)
-  server.listen(80, function () {
+  server.listen(5000, function () {
     console.log('JSON Server is running')
   })
 }
