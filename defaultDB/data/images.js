4f9da9e407e6dc6042147bd64012cb8fa6d57447
atsushi570 <55192393+atsushi570@users.noreply.github.com>
2021-05-04 15:38:26 +0900

    fix static images

--- defaultDB/data/images.js
+++ defaultDB/data/images.js
@@ -3,31 +3,31 @@ const images = [
     'id': 17,
     'relationId': "post001",
     'relationType': 1,
-    'path': 'http://localhost:55555/images/notice1.png'
+    'path': 'https://nameless-fortress-64042.herokuapp.com/images/notice1.png'
   },
   {
     'id': 19,
     'relationId': "post002",
     'relationType': 1,
-    'path': 'http://localhost:55555/images/notice2.png'
+    'path': 'https://nameless-fortress-64042.herokuapp.com/images/notice2.png'
   },
   {
     'id': 20,
     'relationId': "post002",
     'relationType': 1,
-    'path': 'http://localhost:55555/images/notice3.png'
+    'path': 'https://nameless-fortress-64042.herokuapp.com/images/notice3.png'
   },
   {
     'id': 21,
     'relationId': "post002",
     'relationType': 1,
-    'path': 'http://localhost:55555/images/notice4.png'
+    'path': 'https://nameless-fortress-64042.herokuapp.com/images/notice4.png'
   },
   {
     'id': 22,
     'relationId': "post002",
     'relationType': 1,
-    'path': 'http://localhost:55555/images/sample.png'
+    'path': 'https://nameless-fortress-64042.herokuapp.com/images/sample.png'
   },
 ]
 
