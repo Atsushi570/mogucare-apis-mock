ec6144bf282773269a55b1323ed314f6ea49427e
atsushi570 <55192393+atsushi570@users.noreply.github.com>
2021-05-15 17:14:27 +0900

    ポート変更

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ function main() {
 
   // json serverを起動する
   server.use(router)
-  server.listen(55555, function () {
+  server.listen(process.env.PORT || 5000, function () {
     console.log('JSON Server is running')
   })
 }
